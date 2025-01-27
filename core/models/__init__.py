5653eae1b65cd1ec233336453e56e405aaa86473
Vitalii <vitaly.savenko12@gmail.com>
2025-01-27 19:56:30 +0200

    add  migrations user and rename table to s

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,8 +3,10 @@ __all__ = (
     "Product",
     "DatabaseHelper",
     "db_helper",
+    "User"
 )
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .product import Product
+from .user import User
